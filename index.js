6167adf29db53708f801e0ee0b4d4db88923a458
Umang Rathod <75923134+umang00775@users.noreply.github.com>
2022-11-06 15:31:00 +0530

    Navigation designed

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+//User logged in or not
+let isLoggedIn = true;
+
 app.get('/',(req,res)=>{
-    res.send("Hello");
-})
+    if(isLoggedIn){
+        res.render('home.ejs');
+    }
+    else{
+        res.render('signup.ejs');
+    }
+});
+
+
+app.get('/login', (req,res)=>{
+    res.render('login.ejs');
+});
+
 
 app.listen(port,()=>{
-    console.log(`Listening to port ${port}`);
+    console.log(`Yeah bro!! \nI am listening to port ${port}`);
 })
